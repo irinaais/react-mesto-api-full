a0c524ba25778d5746589239676afb6125ad08f9
Iris <irinaais90@yandex.ru>
2022-08-22 17:02:21 +1000

    refactor: change server for ApiAuth.js for frontend

--- frontend/src/utils/ApiAuth.js
+++ frontend/src/utils/ApiAuth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://auth.nomoreparties.co';
+export const BASE_URL = 'http://api.mesto.irinaosipova.nomoredomains.sbs/';
 
 function checkResponse(res) {
   if (res.ok) {
