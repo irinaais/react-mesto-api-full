4542acbeacc9f19d3a796326474a41cefffc5e23
Iris <irinaais90@yandex.ru>
2023-01-09 18:11:27 +0400

    feat: update footer__copyright

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,7 @@
 function Footer() {
   return(
       <footer className="footer page__section">
-        <p className="footer__copyright">© 2022. Ирина Осипова</p>
+        <p className="footer__copyright">© 2022-2023. Ирина Осипова</p>
       </footer>
   );
 }
